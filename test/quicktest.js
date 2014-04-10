c9f3921ee4ebdd53ef29419d46533fad91cf6ce0
Thomas Heigl <thomasheigl2@gmail.com>
2014-04-10 14:37:23 +0200

    Test Script modified

--- test/quicktest.js
+++ test/quicktest.js
@@ -20,7 +20,34 @@ pdfPageCount.count(__dirname + "/example_with_one_page.pdf", function(resp){
 	}
 	
 	if(resp.data == 1) console.log("Yayy, test with one page works!");
-	else console.log("Oh no..tool says the PDF has " + res.data + " pages, but it should say it has one page!");
+	else console.log("Oh no..tool says the PDF has " + resp.data + " pages, but it should say it has one page!");
+});
+
+// test with pdf with one page
+pdfPageCount.count("http://www.ruhr-uni-bochum.de/www-rz/schwanbs/TeX/einfuehrung-in-tex.pdf", function(resp){
+	if(!resp.success)
+	{
+		console.log("Something went wrong: " + resp.error);
+		
+		return;
+	}
+	
+	if(resp.data == 1) console.log("Yayy, test with one page, given through a web url works!");
+	else console.log("Oh no..tool says the PDF has " + resp.data + " pages, but it should say it has one page!");
+});
+
+// test with pdf with one page, but first read the file and give the function the raw data
+var file = fs.readFileSync(__dirname + "/example_with_one_page.pdf");
+
+pdfPageCount.count(file, function(resp){
+	if(!resp.success)
+	{
+		console.log("Something went wrong: " + resp.error);
+		
+		return;
+	}
+	
+	console.log("ONline docuemnt has " + resp.data + " pages!");
 });
 
 // test with PDF with three pages
@@ -33,5 +60,5 @@ pdfPageCount.count(__dirname + "/example_with_three_pages.pdf", function(resp){
 	}
 	
 	if(resp.data == 3) console.log("Yayy, test with multiple pages works!");
-	else console.log("Oh no..tool says the PDF has " + res.data + " pages, but it should say it has three pages!");
+	else console.log("Oh no..tool says the PDF has " + resp.data + " pages, but it should say it has three pages!");
 });
